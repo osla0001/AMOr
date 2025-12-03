14d4d8062a36a114a05aa951e77d642b840c248a
Oscar Laursen <osla0001@stud.kea.dk>
2025-12-03 12:29:26 +0000

    4th try

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,5 @@ import { defineConfig } from "astro/config";
 import netlify from "@astrojs/netlify";
 
 export default defineConfig({
-  integrations: [
-    netlify({
-      // for Netlify Functions (serverless) use default options,
-      // or use netlify({ edge: true }) for edge functions if needed
-    }),
-  ],
+  adapter: netlify(),
 });
